4f6e02f86e8edc468cad2d9a0775a91b5cfecf86
Grazvy <Grazvydas.Kuras@gmail.com>
2025-07-10 14:09:33 +0200

    implemented x-axis customisation

--- src/WPMGraph.py
+++ src/WPMGraph.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 import numpy as np
 from PyQt6.QtCore import Qt, QTimer
-from PyQt6.QtWidgets import QFrame, QVBoxLayout, QSizePolicy
+from PyQt6.QtWidgets import QFrame, QVBoxLayout, QSizePolicy, QPushButton, QComboBox
 from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 
@@ -17,12 +17,23 @@ class WPMGraph(QFrame):
         self.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Fixed)
 
         layout = QVBoxLayout(self)
+
+        self.back_to_start = QPushButton(f"back to current time", self)
+        self.back_to_start.clicked.connect(self.reset_position)
+        layout.addWidget(self.back_to_start)
+
+        self.combo = QComboBox()
+        self.combo.addItems(["1 min", "5 min", "15 min", "30 min", "60 min"])
+        self.combo.currentTextChanged.connect(self.update_mult)
+        layout.addWidget(self.combo)
+
         self.canvas = FigureCanvas(Figure(figsize=(8, 4)))
         layout.addWidget(self.canvas)
 
         self.canvas.setFocusPolicy(Qt.FocusPolicy.StrongFocus)
         self.canvas.setFocus()
 
+        self.mult = 1    # normalized to one minute
         self.custom_interval = False
         self.db = db
         self.bin_size = bin_size
@@ -35,13 +46,13 @@ class WPMGraph(QFrame):
         # auto-refresh
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.update_plot)
-        self.timer.start(10_000)
+        self.timer.start(5_000)
 
         self.canvas.mpl_connect("scroll_event", self.on_scroll)
 
     def resizeEvent(self, event):
         pixels = self.width()
-        self.interval_size = int(pixels * SPP)
+        self.interval_size = int(pixels * SPP) * self.mult
 
         self.plot()
         super().resizeEvent(event)
@@ -54,11 +65,36 @@ class WPMGraph(QFrame):
     def on_scroll(self, event):
         self.custom_interval = True
         direction = event.step  # +1 for up, -1 for down
-        self.interval_end += -direction
+        self.interval_end += -direction * self.mult
+        self.plot()
+
+    def reset_position(self):
+        self.custom_interval = False
+        self.interval_end = time.time()
         self.plot()
 
     def get_last_bin(self):
         return (self.interval_end // self.bin_size) * self.bin_size
+
+    def update_mult(self, text):
+        if text == "1 min":
+            new_mult = 1
+        elif text == "5 min":
+            new_mult = 5
+        elif text == "15 min":
+            new_mult = 15
+        elif text == "30 min":
+            new_mult = 30
+        elif text == "60 min":
+            new_mult = 60
+        else:
+            raise ValueError(f"Unrecognized text {text}")
+
+        self.bin_size = (self.bin_size // self.mult) * new_mult
+        self.interval_size = (self.interval_size // self.mult) * new_mult
+        self.mult = new_mult
+        self.plot()
+
 
     def plot(self):
         last_bin = self.get_last_bin()
@@ -82,7 +118,6 @@ class WPMGraph(QFrame):
         self.canvas.figure.clear()
         ax = self.canvas.figure.add_subplot(111)
 
-        # Plot bars even if all values are NaN
         x_vals = np.array(all_bin_centers)
         y_vals = np.array(all_wpm_values)
         valid_mask = ~np.isnan(y_vals)
@@ -95,19 +130,26 @@ class WPMGraph(QFrame):
 
         label_positions = []
         label_strings = []
-        seen_minutes = set()
+        seen_buckets = set()
 
         for ts in all_bin_centers:
             dt = datetime.fromtimestamp(ts)
-            minute_key = (dt.hour, dt.minute)
 
-            if minute_key not in seen_minutes and dt.second < self.bin_size:
-                seen_minutes.add(minute_key)
+            bucket_minute = (dt.minute // self.mult) * self.mult
+            bucket_key = (dt.hour, bucket_minute)
+
+            if bucket_key not in seen_buckets:
+                seen_buckets.add(bucket_key)
                 label_positions.append(ts)
                 label_strings.append(dt.strftime('%H:%M'))
 
-        ax.set_xticks(label_positions)
-        ax.set_xticklabels(label_strings, rotation=45, ha='right')
+        if label_positions[1] - label_positions[0] < 60 * self.mult:
+            # remove first label, if too close
+            ax.set_xticks(label_positions[1:])
+            ax.set_xticklabels(label_strings[1:], rotation=45, ha='right')
+        else:
+            ax.set_xticks(label_positions)
+            ax.set_xticklabels(label_strings, rotation=45, ha='right')
 
         ax.set_title('Words Per Minute', fontsize=14, fontweight='bold')
         ax.grid(axis='y', alpha=0.6, linewidth=1.2, color='gray')
@@ -115,5 +157,5 @@ class WPMGraph(QFrame):
         y_max = self.db.get_max() * 1.25
         ax.set_ylim(0, y_max)
 
-        self.canvas.figure.tight_layout()
+        self.canvas.figure.tight_layout
         self.canvas.draw()
